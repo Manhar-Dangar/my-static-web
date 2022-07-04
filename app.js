1016d41c13f9af4d15ef8190a912aa956049d7d7
Manhar-Dangar <73153726+Manhar-Dangar@users.noreply.github.com>
2022-07-04 14:35:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const APP_PORT = process.env.APP_PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello Node')
-})
+}) 
 app.listen(APP_PORT, () => { 
   console.log(`Running app at porfdfdft:${APP_PORT}`)
 })  
